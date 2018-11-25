030d7fc0d0da000d03bd95daeecc576c4ac52e96
Рустам <r.motigullin@key-g.com>
2018-11-25 20:28:07 +0300

    Initial commit

---
+++ Example/ContainerView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ContainerView
+//
+//  Created by rustamburger@gmail.com on 11/25/2018.
+//  Copyright (c) 2018 rustamburger@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+#import "ContainerViewAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ContainerViewAppDelegate class]));
+    }
+}
